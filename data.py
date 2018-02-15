a8c7d373338d96f3b0ac8a19d0f1f08bb63ff88e
Nidhi <nidhi0613@gmail.com>
2018-02-15 01:49:58 -0500

    Update Load Data

--- data.py
+++ data.py
@@ -20,15 +20,18 @@ class Data(object):
         self.box = table
         self.sent = sentence
 
-    def add_data(table, sentence):
+    def add_data(self, table, sentence):
         self.box = table
         self.sent = sentence
+
 
 
 class Corpus(object):
     def __init__(self, path):
         self.dictionary = Dictionary()
-
+        self.train = []
+        self.val = []
+        self.test = []
         file = open(os.path.join(path, 'train/train.sent'), "r")
         trainsent = [line.split('\n')[0] for line in file]
         file = open(os.path.join(path, 'train/train.nb'), "r")
@@ -52,32 +55,48 @@ class Corpus(object):
 
         self.train, self.val, self.test = self.build_data(trainsent, trainnb, testsent, testnb, valsent, valnb, traintab, testtab, valtab)
         # TODO: tokenize each of these and update here , should return updated dictionary, and tokenized text
-        self.tokenize()
+        self.tokenize_common()
 
 
-    def tokenize(self):
-        self.dictionary
+    # Function to have a common dictionary for tables and biography texts
+    def tokenize_common(self):
+        # Add words to the dictionary
+        tokens = 0
+        for t in self.train:
+            content = t.box.split('\t')
+            for c in content:
+                parts = c.split(":")
+                for p in parts:
+                    tokens += 1
+                    self.dictionary.add_word(p)
+            content = t.sent.split('\n')
+            for c in content:
+                words = c.split() + ['<eos>']
+                tokens += len(words)
+                for word in words:
+                    self.dictionary.add_word(word)
+
+        # Tokenize file
+        # TODO
+
+
+
+
+
 
 
 
     def build_data(self, trainsent, trainnb, testsent, testnb, valsent, valnb, traintab, testtab, valtab):
         k = 0
         for i in range(0,len(trainnb)):
-            self.train.append(Data(traintab[i],' '.join(trainsent[k:k+int(trainnb[i].split()[0])])))
+            self.train.append(Data(traintab[i],'\n'.join(trainsent[k:k+int(trainnb[i].split()[0])])))
             k = k+int(trainnb[i].split()[0])
         k = 0
         for i in range(0,len(testnb)):
-            self.test.append(Data(testtab[i],' '.join(testsent[k:k+int(testnb[i].split()[0])])))
+            self.test.append(Data(testtab[i],'\n'.join(testsent[k:k+int(testnb[i].split()[0])])))
             k = k+int(testnb[i].split()[0])
         k = 0
         for i in range(0,len(valnb)):
-            self.val.append(Data(valtab[i],' '.join(valsent[k:k+int(valnb[i].split()[0])])))
+            self.val.append(Data(valtab[i],'\n'.join(valsent[k:k+int(valnb[i].split()[0])])))
             k = k+int(valnb[i].split()[0])
         return self.train, self.val, self.test
-
-
-
-        # for every line in the file
-            # create a new object and add it to the file
-        #return self.train, self.val, self.test
-        # train is a list of objects with train[0].box being the table and train[0].sent being the sentence

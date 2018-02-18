40124f94e432fa5ad7b4992f14de3960e69f7ff3
Nidhi <nidhi0613@gmail.com>
2018-02-18 17:34:38 -0500

    Module Batchify

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torchwordemb
 import argparse
 import torch
 import data as data
+from batchify_pad import batchify, pad_collate
 ###############################################################################
 # Parse command line arguments
 ###############################################################################
@@ -43,64 +44,8 @@ print('Train:', len(corpus.train), 'Validation:', len(corpus.val), 'Test:', len(
 # TODO: Padding
 # TODO: Stacking
 ###############################################################################
-def batchify(corpus):
-    generate = 1 # generates only the top k sentence of the biography
-
-    train_box = [] # List of tokensized sequences
-    train_sent = []
-    val_box = []
-    val_sent = []
-    test_box = []
-    test_sent = []
-    for example in corpus.train:
-        temp = []
-        for index in example.box.split('\t'):
-            for i in index.split(':'):
-                temp.append(i)
-        train_box.append(temp)
-
-        temp = []
-        for index in range(0,len(example.sent.split('\n'))):
-            if index<=generate-1:
-                for i in example.sent.split('\n')[index].split():
-                    temp.append(i)
-        train_sent.append(temp)
-
-    for example in corpus.val:
-        temp = []
-        for index in example.box.split('\t'):
-            for i in index.split(':'):
-                temp.append(i)
-        val_box.append(temp)
-
-        temp = []
-        for index in range(0,len(example.sent.split('\n'))):
-            if index<=generate-1:
-                for i in example.sent.split('\n')[index].split():
-                    temp.append(i)
-        val_sent.append(temp)
-
-    for example in corpus.test:
-        temp = []
-        for index in example.box.split('\t'):
-            for i in index.split(':'):
-                temp.append(i)
-        test_box.append(temp)
-
-        temp = []
-        for index in range(0,len(example.sent.split('\n'))):
-            if index<=generate-1:
-                for i in example.sent.split('\n')[index].split():
-                    temp.append(i)
-        test_sent.append(temp)
-
-    return train_box, train_sent, val_box, val_sent, test_box, test_sent
-#def pad_collate(train_box, train_sent, val_box, val_sent, test_box, test_sent):
-
-
-train_box, train_sent, val_box, val_sent, test_box, test_sent = Batchify(corpus)
-#train_box, train_sent, val_box, val_sent, test_box, test_sent = pad_collate(train_box, train_sent, val_box, val_sent, test_box, test_sent)
-
+train_box, train_sent, val_box, val_sent, test_box, test_sent = batchify(corpus)
+train_box, train_sent, val_box, val_sent, test_box, test_sent = pad_collate(train_box, train_sent, val_box, val_sent, test_box, test_sent)
 ###############################################################################
 # TODO: Build Model
 # Train Model

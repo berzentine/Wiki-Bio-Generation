ad105d2180e62cc02cf986d6c61bb886a55527af
Nidhi <nidhi0613@gmail.com>
2018-02-18 20:03:22 -0500

    Add Stacking

--- batchify_pad.py
+++ batchify_pad.py
@@ -1,6 +1,8 @@
+import torch
+import numpy as np
+
 def batchify(corpus):
     generate = 1 # generates only the top k sentence of the biography
-
     train_box = [] # List of tokensized sequences
     train_sent = []
     val_box = []
@@ -50,8 +52,9 @@ def batchify(corpus):
         test_sent.append(temp)
 
     return train_box, train_sent, val_box, val_sent, test_box, test_sent
-def pad_collate(train_box, train_sent, val_box, val_sent, test_box, test_sent,batchsize):
-    train =  sorted(zip(train_sent,train_box), key=lambda pair: len(pair[0]))
+def pad_collate(datum,batchsize):
+    batchsize = 3
+    train =  sorted(zip(datum[1],datum[0]), key=lambda pair: len(pair[0]))
     train_box = []
     train_sent = []
     for i in range(0,len(train),batchsize):
@@ -66,55 +69,11 @@ def pad_collate(train_box, train_sent, val_box, val_sent, test_box, test_sent,ba
         for b in batch:
             while(len(b[0])<max_sent):
                 b[0].append(0)
-            temp_sent.append(b[0])
+            b= ([int(a) for a in b[0]],[int(a) for a in b[1]])
+            temp_sent.append(torch.LongTensor(b[0]))
             while(len(b[1])<max_box):
                 b[1].append(0)
-            temp_box.append(b[1])
-        train_box.append(temp_box)
-        train_sent.append(temp_sent)
-
-    val =  sorted(zip(val_sent,val_box), key=lambda pair: len(pair[0]))
-    val_box = []
-    val_sent = []
-    for i in range(0,len(val),batchsize):
-        batch = val[i:i+batchsize]
-        max_sent = len(batch[-1][0])
-        max_box = -100
-        for b in batch:
-            if max_box<len(b[1]):
-                max_box=len(b[1])
-        temp_box = []
-        temp_sent = []
-        for b in batch:
-            while(len(b[0])<max_sent):
-                b[0].append(0)
-            temp_sent.append(b[0])
-            while(len(b[1])<max_box):
-                b[1].append(0)
-            temp_box.append(b[1])
-        val_box.append(temp_box)
-        val_sent.append(temp_sent)
-
-    test =  sorted(zip(test_sent,test_box), key=lambda pair: len(pair[0]))
-    test_box = []
-    test_sent = []
-    for i in range(0,len(test),batchsize):
-        batch = test[i:i+batchsize]
-        max_sent = len(batch[-1][0])
-        max_box = -100
-        for b in batch:
-            if max_box<len(b[1]):
-                max_box=len(b[1])
-        temp_box = []
-        temp_sent = []
-        for b in batch:
-            while(len(b[0])<max_sent):
-                b[0].append(0)
-            temp_sent.append(b[0])
-            while(len(b[1])<max_box):
-                b[1].append(0)
-            temp_box.append(b[1])
-        test_box.append(temp_box)
-        test_sent.append(temp_sent)
-
-    return train_box, train_sent, val_box, val_sent, test_box, test_sent
+            temp_box.append(torch.LongTensor(b[1]))
+        train_box.append(torch.stack(temp_box, dim=0))
+        train_sent.append(torch.stack(temp_sent, dim=0))
+    return train_box, train_sent

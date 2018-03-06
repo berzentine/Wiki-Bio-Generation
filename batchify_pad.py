d3472c90265bd096bae5a82403732b4f341e136d
Nidhi <nidhi0613@gmail.com>
2018-03-05 22:20:26 -0500

    fixed batchify data_handler, added sanity check for them

--- batchify_pad.py
+++ batchify_pad.py
@@ -52,15 +52,12 @@ def batchify(corpus, batchsize):
     [valid_value_length, valid_field_length, valid_ppos_length, valid_pneg_length, valid_sent_length]]
 
 
+
     for i in range(0,3):
-        datum = sorted((data_store[i][4], data_store[i][0], data_store[i][1], data_store[i][2], data_store[i][3]), key=lambda tup: len(tup[0]))
+        if i ==1: print 'Done batchifying train data'
+        if i ==2: print 'Done batchifying test data'
+        datum = sorted(zip(data_store[i][4], data_store[i][0], data_store[i][1], data_store[i][2], data_store[i][3]), key=lambda tup: len(tup[0]))
         for d in range(0,len(datum), batchsize):
-            batch = datum[d:d+batchsize] # create a batch
-            table_max_length = -100
-            for b in batch: # find biggest table
-                if table_max_length<len(b[1]):
-                    table_max_length=len(b[1])
-            max_sent = len(batch[-1][0])
             temp_sentences_actual_length = []
             temp_sentences_padded = []
             temp_table_field_actual_length = []
@@ -71,20 +68,29 @@ def batchify(corpus, batchsize):
             temp_table_ppos_padded = []
             temp_table_pneg_actual_length = []
             temp_table_pneg_padded = []
-            for b in batch:
-                # padd sentences in batch
+            # padd sentences in batch
+            batch_sent = datum[d:d+batchsize]
+            max_sent = len(batch_sent[-1][0])
+            for b in batch_sent:
                 temp_sentences_actual_length.append(len(b[0]))
                 while(len(b[0])<max_sent):
                     b[0].append(0)
                 temp_sentences_padded.append(torch.LongTensor(b[0]))
-                # padd remaining items in the batch
-                # padd values in batch # padd fields in batch # padd positions in batch
-                # can be done toegther since same length of these all
+            data_length[i][4].append(temp_sentences_actual_length)
+            data_padded[i][4].append(torch.stack(temp_sentences_padded, dim=0))
+            # padd remaining items in the batch
+            # padd values in batch # padd fields in batch # padd positions in batch
+            # can be done toegther since same length of these all
+            table_max_length = -100
+            for b in batch_sent: # find biggest table
+                if table_max_length<len(b[1]):
+                    table_max_length=len(b[1])
+            for b in batch_sent:
                 temp_table_value_actual_length.append(len(b[1]))
                 temp_table_field_actual_length.append(len(b[2]))
                 temp_table_ppos_actual_length.append(len(b[3]))
                 temp_table_pneg_actual_length.append(len(b[4]))
-                """while(len(b[1])<table_max_length):
+                while(len(b[1])<table_max_length): # done based on value
                     b[1].append(0) # value
                     b[2].append(0) # field
                     b[3].append(0) # ppos
@@ -92,17 +98,16 @@ def batchify(corpus, batchsize):
                 temp_table_pneg_padded.append(torch.LongTensor(b[4]))
                 temp_table_ppos_padded.append(torch.LongTensor(b[3]))
                 temp_table_field_padded.append(torch.LongTensor(b[2]))
-                temp_table_value_padded.append(torch.LongTensor(b[1]))"""
+                temp_table_value_padded.append(torch.LongTensor(b[1]))
             # append padded sentences and their lengths in final batch
-            data_padded[i][4].append(torch.stack(temp_sentences_padded, dim=0))
             data_padded[i][3].append(torch.stack(temp_table_pneg_padded, dim=0))
             data_padded[i][2].append(torch.stack(temp_table_ppos_padded, dim=0))
             data_padded[i][1].append(torch.stack(temp_table_field_padded, dim=0))
             data_padded[i][0].append(torch.stack(temp_table_value_padded, dim=0))
-
-            data_length[i][4].append(temp_sentences_actual_length)
             data_length[i][3].append(temp_table_pneg_actual_length)
             data_length[i][2].append(temp_table_ppos_actual_length)
             data_length[i][1].append(temp_table_field_actual_length)
             data_length[i][0].append(temp_table_value_actual_length)
+        #break
+        print 'Done batchifying valid data'
     return data_padded, data_length

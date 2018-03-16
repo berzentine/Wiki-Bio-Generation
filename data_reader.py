fcf4c8d4d6ed510f2ad321c8f0e187aded86c079
Vidhisha Balachandran <vidhishabalachandran@Vidhishas-MacBook-Pro.local>
2018-03-16 11:02:49 -0400

    data reader changed

--- data_reader.py
+++ data_reader.py
@@ -37,9 +37,14 @@ class Corpus(object):
         self.train_field_dict = Dictionary()
         self.train_value_dict = Dictionary()
         self.train_ppos = []
+        self.train_ppos_len = []
         self.train_pneg = []
+        self.train_pneg_len = []
         self.train_field = []
+        self.train_field_len = []
         self.train_value = []
+        self.train_value = []
+        self.train_sent = []
         self.train_sent = []
 
         self.test_ppos_dict = Dictionary()
@@ -47,22 +52,32 @@ class Corpus(object):
         self.test_field_dict = Dictionary()
         self.test_value_dict = Dictionary()
         self.test_sent_dict = Dictionary()
-        self.test_sent = []
         self.test_ppos = []
+        self.test_ppos_len = []
         self.test_pneg = []
+        self.test_pneg_len = []
         self.test_field = []
+        self.test_field_len = []
         self.test_value = []
+        self.test_value = []
+        self.test_sent = []
+        self.test_sent = []
 
         self.valid_ppos_dict = Dictionary()
         self.valid_pneg_dict = Dictionary()
         self.valid_field_dict = Dictionary()
         self.valid_value_dict = Dictionary()
         self.valid_sent_dict = Dictionary()
-        self.valid_sent = []
         self.valid_ppos = []
+        self.valid_ppos_len = []
         self.valid_pneg = []
+        self.valid_pneg_len = []
         self.valid_field = []
+        self.valid_field_len = []
         self.valid_value = []
+        self.valid_value = []
+        self.valid_sent = []
+        self.valid_sent = []
         self.verbose = verbose
 
         self.data_path = [['train/train.sent', 'train/train.nb', 'train/train.box'], \
@@ -71,15 +86,26 @@ class Corpus(object):
 
         # self.train = {"field_value" = }
 
-        self.data_store = [[self.train_value, self.train_field , self.train_ppos, self.train_pneg, self.train_sent],\
-        [self.test_value, self.test_field , self.test_ppos, self.test_pneg, self.test_sent],\
-        [self.valid_value, self.valid_field , self.valid_ppos, self.valid_pneg, self.valid_sent]]
+        # self.data_store = [[self.train_value, self.train_field , self.train_ppos, self.train_pneg, self.train_sent],\
+        # [self.test_value, self.test_field , self.test_ppos, self.test_pneg, self.test_sent],\
+        # [self.valid_value, self.valid_field , self.valid_ppos, self.valid_pneg, self.valid_sent]]
+        #
+        # self.data_dict = [[self.train_value_dict, self.train_field_dict , self.train_ppos_dict, self.train_pneg_dict, self.train_sent_dict],\
+        # [self.test_value_dict, self.test_field_dict , self.test_ppos_dict, self.test_pneg_dict, self.test_sent_dict],\
+        # [self.valid_value_dict, self.valid_field_dict , self.valid_ppos_dict, self.valid_pneg_dict, self.valid_sent_dict]]
+        #
+        # self.populate_stores(path, top_k, verbose)
+        self.train_value_dict, self.train_value, self.train_field_dict, self.train_field,\
+        self.train_ppos_dict, self.train_ppos, self.train_pneg_dict, self.train_pneg,\
+        self.train_sent_dict, self.train_sent = self.new_populate_stores(path, self.data_path[0], top_k, verbose)
 
-        self.data_dict = [[self.train_value_dict, self.train_field_dict , self.train_ppos_dict, self.train_pneg_dict, self.train_sent_dict],\
-        [self.test_value_dict, self.test_field_dict , self.test_ppos_dict, self.test_pneg_dict, self.test_sent_dict],\
-        [self.valid_value_dict, self.valid_field_dict , self.valid_ppos_dict, self.valid_pneg_dict, self.valid_sent_dict]]
+        self.test_value_dict, self.test_value, self.test_field_dict, self.test_field, \
+        self.test_ppos_dict, self.test_ppos, self.test_pneg_dict, self.test_pneg, \
+        self.test_sent_dict, self.test_sent = self.new_populate_stores(path, self.data_path[1], top_k, verbose)
 
-        self.populate_stores(path, top_k, verbose)
+        self.valid_value_dict, self.valid_value, self.valid_field_dict, self.valid_field, \
+        self.valid_ppos_dict, self.valid_ppos, self.valid_pneg_dict, self.valid_pneg, \
+        self.valid_sent_dict, self.valid_sent = self.new_populate_stores(path, self.data_path[2], top_k, verbose)
 
     def populate_stores(self, path, top_k, verbose):
         for i in range(0, 3):
@@ -142,3 +168,69 @@ class Corpus(object):
                 self.data_store[i][3].append(temp_pneg)
                 # break
         if verbose: print('Done loading val. data')
+
+    def new_populate_stores(self, path, data_path, top_k, verbose):
+        # handle the sentences # handle the nb # tokenize the appendings
+        file = open(os.path.join(path, data_path[0]), "r")
+        sentences = [line.split('\n')[0] for line in file]
+        file = open(os.path.join(path, data_path[1]), "r")
+        no_sentences = [int(line.split('\n')[0]) for line in file]
+        sent_dict = Dictionary()
+        value_dict = Dictionary()
+        field_dict = Dictionary()
+        ppos_dict = Dictionary()
+        pneg_dict = Dictionary()
+        sent = []
+        ppos = []
+        pneg = []
+        field = []
+        value = []
+        sent_dict.add_word('<pad>')
+        z = 0
+        for s in range(len(no_sentences)):
+            current = sentences[z:z + no_sentences[s]]
+            z = z + no_sentences[s]
+            current = current[0:top_k]
+            temp_sent = []
+            for c in current:
+                c = ['<sos>'] + c.split(' ') + ['<eos>']
+                # print c
+                for word in c:
+                    sent_dict.add_word(word)
+                    temp_sent.append(sent_dict.word2idx[word])
+                    # print temp_sent
+                    # print '*'*32
+            sent.append(temp_sent)
+
+        # handle the table and tokenize
+        value_dict.add_word('<pad>')
+        field_dict.add_word('<pad>')
+        ppos_dict.add_word('<pad>')
+        pneg_dict.add_word('<pad>')
+        file = open(os.path.join(path, data_path[2]), "r")
+        for line in file:
+            temp_ppos = []
+            temp_pneg = []
+            temp_field = []
+            temp_value = []
+            line = line.split('\n')[0].split('\t')
+            j = 0
+            for l in line:  # address each part in the table for f, p+, p-, and value
+                field_dict.add_word(l.split(':')[0])
+                temp_field.append(field_dict.word2idx[l.split(':')[0]])
+
+                value_dict.add_word(l.split(':')[1])
+                temp_value.append(value_dict.word2idx[l.split(':')[1]])
+
+                ppos_dict.add_word(j)
+                temp_ppos.append(ppos_dict.word2idx[j])
+
+                pneg_dict.add_word(len(line) - j - 1)
+                temp_pneg.append(pneg_dict.word2idx[len(line) - j - 1])
+                j += 1
+
+            value.append(temp_value)
+            field.append(temp_field)
+            ppos.append(temp_ppos)
+            pneg.append(temp_pneg)
+        return value_dict, value, field_dict, field, ppos_dict, ppos, pneg_dict, pneg, sent_dict, sent

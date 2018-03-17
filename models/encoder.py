360ecb7cc54a8e1fee20a1210361486975d5e9cf
Nidhi <nidhi0613@gmail.com>
2018-03-16 21:58:38 -0400

    Encoder forward update

--- models/encoder.py
+++ models/encoder.py
@@ -10,35 +10,50 @@ class Encoder(nn.Module):
         self.batch_size = batch_size
         self.bias = bias
         self.z_size = z_size
-        # Weights W_*h
-        self.weight_ih = Parameter(torch.Tensor(hidden_size, hidden_size))
-        self.weight_oh = Parameter(torch.Tensor(hidden_size, hidden_size))
-        self.weight_fh = Parameter(torch.Tensor(hidden_size, hidden_size))
-        self.weight_ctildeh = Parameter(torch.Tensor(hidden_size, hidden_size))
         # Weights W_*x
-        self.weight_ix = Parameter(torch.Tensor(embed_size, hidden_size))
-        self.weight_ox = Parameter(torch.Tensor(embed_size, hidden_size))
-        self.weight_fx = Parameter(torch.Tensor(embed_size, hidden_size))
-        self.weight_ctildex = Parameter(torch.Tensor(embed_size, hidden_size))
-        self.weight_lx = Parameter(torch.Tensor(z_size, hidden_size))
-        self.weight_zhatx = Parameter(torch.Tensor(z_size, hidden_size))
-        # Bias
-        if bias:
-            self.bias_i = Parameter(torch.Tensor(hidden_size))
-            self.bias_f = Parameter(torch.Tensor(hidden_size))
-            self.bias_o = Parameter(torch.Tensor(hidden_size))
-            self.bias_l = Parameter(torch.Tensor(hidden_size))
-            self.bias_zhat = Parameter(torch.Tensor(hidden_size))
-            self.bias_ctilde = Parameter(torch.Tensor(hidden_size))
-        else:
-            self.register_parameter('bias_i', None)
-            self.register_parameter('bias_f', None)
-            self.register_parameter('bias_o', None)
-            self.register_parameter('bias_l', None)
-            self.register_parameter('bias_zhat', None)
-            self.register_parameter('bias_ctilde', None)
+        self.input_weights = nn.Linear(embed_size, 4 * hidden_size)
+        #self.weight_ix = Parameter(torch.Tensor(embed_size, hidden_size))
+        #self.weight_ox = Parameter(torch.Tensor(embed_size, hidden_size))
+        #self.weight_fx = Parameter(torch.Tensor(embed_size, hidden_size))
+        #self.weight_ctildex = Parameter(torch.Tensor(embed_size, hidden_size))
 
-    def forward(self, input, hidden, cell_state): # input vector, h_0 intialized as 0's and same for cell state
+        # Weights W_*h
+        self.hidden_weights = nn.Linear(hidden_size, 4 * hidden_size)
+        #self.weight_ih = Parameter(torch.Tensor(hidden_size, hidden_size))
+        #self.weight_oh = Parameter(torch.Tensor(hidden_size, hidden_size))
+        #self.weight_fh = Parameter(torch.Tensor(hidden_size, hidden_size))
+        #self.weight_ctildeh = Parameter(torch.Tensor(hidden_size, hidden_size))
+
+        # Weights z_t
+        self.z_weights = nn.Linear(z_size, 2 * hidden_size)
+        #self.weight_lx = Parameter(torch.Tensor(z_size, hidden_size))
+        #self.weight_zhatx = Parameter(torch.Tensor(z_size, hidden_size))
+
+    # Always batch first is needed
+    def forward(self, input_d, input_z, hidden, cell_state): # input vector, h_0 intialized as 0's and same for cell state
         def recurrence(d_t, z_t, h_t_1, c_t_1):
-            i_t = F.sigmoid(F.linear(h_t_1, self.weight_ih, None) + F.linear(d_t, self.weight_ix, None) + self.bias_i)
-            return d_t_1, z_t_1, h_t, c_t
+            gates_vanilla = self.input_weights(d_t) + self.hidden_weights(h_t_1)
+            ingate, forgetgate, cellgate, outgate = gates_vanilla.chunk(4, 1)
+            gates_field = self.z_weights(z_t)
+            lgate, zhatgate = gates_field.chunk(2, 1)
+
+            ingate = F.sigmoid(ingate)
+            forgetgate = F.sigmoid(forgetgate)
+            cellgate = F.tanh(cellgate)
+            outgate = F.sigmoid(outgate)
+            lgate = F.sigmoid(lgate)
+            zhatgate = F.tanh(zhatgate)
+
+            c_t = (forgetgate * c_t_1) + (ingate * cellgate) + (lgate * zhatgate)
+            h_t = outgate * F.tanh(c_t)
+
+            return h_t, c_t
+
+        output = []
+        steps = range(input_d.size(1))  # input_d = batch X seq_length X dim
+        for i in steps:
+            hidden, cell_state = recurrence(input_d[i], input_z[i], hidden, cell_state)  # TODO: change here
+            output.append((hidden, cell_state))  # TODO change here
+
+        output = torch.cat(output, 0).view(input.size(0), *output[0].size()) # TODO change here
+        return output, hidden, cell_state

ce1e78ee473d2ea834d8f82a53a9d5510ecae55e
Vidhisha Balachandran <vbalacha@arnout.ml.cmu.edu>
2018-03-21 19:24:46 -0400

    fixed e2e running, added ignore index

--- models/joint_model.py
+++ models/joint_model.py
@@ -6,7 +6,7 @@ from models.encoder import Encoder
 
 class Seq2SeqModel(nn.Module):
     def __init__(self, sent_vocab_size, field_vocab_size, ppos_vocab_size, pneg_vocab_size, value_vocab_size, sent_embed_size, field_embed_size,\
-                 value_embed_size, ppos_embed_size, pneg_embed_size, encoder_hiiden_size, decoder_hiiden_size, decoder_num_layer, verbose, cuda):
+                 value_embed_size, ppos_embed_size, pneg_embed_size, encoder_hiiden_size, decoder_hiiden_size, decoder_num_layer, verbose, cuda_var):
         super(Seq2SeqModel, self).__init__()
         self.sent_lookup = nn.Embedding(sent_vocab_size, sent_embed_size)
         self.value_lookup = nn.Embedding(value_vocab_size, value_embed_size)
@@ -17,14 +17,14 @@ class Seq2SeqModel(nn.Module):
         self.encoder = Encoder(self.field_rep_embed_size, encoder_hiiden_size, value_embed_size, verbose)
         self.decoder = Decoder(sent_embed_size, decoder_hiiden_size, encoder_hiiden_size, decoder_num_layer, sent_vocab_size, self.field_rep_embed_size, verbose)
         self.verbose = verbose
-        self.cuda = cuda
+        self.cuda_var = cuda_var
 
     def forward(self, sent, value, field, ppos, pneg, batch_size, hidden_dim_encoder):
         input_d = self.value_lookup(value)
         input_z = torch.cat((self.field_lookup(field), self.ppos_lookup(ppos), self.pneg_lookup(pneg)), 2)
         sent = self.sent_lookup(sent)
         encoder_initial_hidden = self.encoder.init_hidden(batch_size, hidden_dim_encoder)
-        if self.cuda:
+        if self.cuda_var:
             encoder_initial_hidden = encoder_initial_hidden.cuda()
         encoder_output, encoder_hidden = self.encoder.forward(input_d=input_d, input_z=input_z, hidden=encoder_initial_hidden)
         encoder_output = torch.stack(encoder_output, dim=0)

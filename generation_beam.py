96e110ab6f7c06fe33b7cb5af553c56d46ffe4bb
Nidhi <nidhi0613@gmail.com>
2018-03-30 22:24:08 -0400

    Generation Beam code minor changes3

--- generation_beam.py
+++ generation_beam.py
@@ -172,6 +172,8 @@ def generate(value, value_len, field, ppos, pneg, batch_size, \
         temp_scores, temp_hiddens , temp_inputs, temp_outputs = [], [], [], [] # store K ones here for each jth exploration in outputs
         for j in range(beam): # explore outputs[j] which is
             curr_input = model.sent_lookup(Variable(torch.LongTensor(1,1).fill_(outputs[j])))
+            if cuda:
+                curr_input = curr_input.cuda()
             decoder_output, attn_vector , prev_hidden = getDecoder(curr_input, hiddens[j], encoder_output)
             values, indices = torch.topk(torch.log(decoder_output)+scores[j], beam, 2)
             for p in range(beam): # append to temp_scores and all temp vectors the top k of outputs of [j]

659747564c53b00e0b1df6bb7dacb2ea215c8cb5
Vidhisha Balachandran <vbalacha@marten.ml.cmu.edu>
2018-04-24 22:11:35 -0400

    changes in generate.sh

--- generate.sh
+++ generate.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-CUDA_AVAILABLE_DEVICES=0 python generation_dual_new_model_alignments_timestep.py \
+CUDA_AVAILABLE_DEVICES=0 python generation_dual_new_model.py \
  --data ./data/Wiki-Data/wikipedia-biography-dataset-debug \
- --model_save_path saved_models/dual_alignment \
- --limit 0.005 \
+ --limit 0.05 \
  --ref_path debug_ref.txt \
  --gen_path debug_gen.txt \
- --unk_gen_path debug_unk.txt
+ --unk_gen_path debug_unk.txt \
+ --use_pickle \
+ --cuda

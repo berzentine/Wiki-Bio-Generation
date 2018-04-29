d2035e95b33db9578d838d9acce08fad02af5036
Vidhisha Balachandran <vbalacha@saskia.ml.cmu.edu>
2018-04-28 20:51:26 -0400

    non working changes

--- generate_alignments.sh
+++ generate_alignments.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 CUDA_AVAILABLE_DEVICES=0 python generation_dual_new_model_alignments.py \
- --data ./data/Wiki-Data/wikipedia-biography-dataset-debug \
+ --data ./data/Wiki-Data/wikipedia-biography-dataset \
  --limit 0.05 \
- --ref_path debug_ref.txt \
- --gen_path debug_gen.txt \
- --unk_gen_path debug_unk.txt \
+ --model_save_path ./saved_models/best_model_timestep_changed.pth \
+ --ref_path full_ref_al.txt \
+ --gen_path full_gen_al.txt \
+ --unk_gen_path full_unk_al.txt \
  --use_pickle \
  --cuda

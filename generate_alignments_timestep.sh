e8a271831a71130c1aa9a771be85904c4dc2c4d6
Vidhisha Balachandran <vbalacha@arnout.ml.cmu.edu>
2018-04-30 18:39:11 -0400

    modifying alignments

--- generate_alignments_timestep.sh
+++ generate_alignments_timestep.sh
@@ -6,6 +6,7 @@ CUDA_AVAILABLE_DEVICES=0 python generation_dual_new_model_alignments_timestep.py
  --limit 0.05 \
  --cuda \
  --use_pickle \
+ --alignments ./data/Wiki-Data/alignments/ibm_model1_alignments.txt \
  --ref_path debug_ref_al_t.txt \
  --gen_path debug_gen_al_t.txt \
  --unk_gen_path debug_unk_al_t.txt

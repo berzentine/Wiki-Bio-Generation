a42f5ff5ec16074d337a306da9e4687276351d0c
Vidhisha Balachandran <vbalacha@arnout.ml.cmu.edu>
2018-05-01 11:21:16 -0400

    changes in generate

--- generate_alignments_timestep.sh
+++ generate_alignments_timestep.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-CUDA_AVAILABLE_DEVICES=0 python generation_dual_new_model_alignments_timestep.py \
+CUDA_AVAILABLE_DEVICES=1 python generation_dual_new_model_alignments_timestep.py \
  --data ./data/Wiki-Data/wikipedia-biography-dataset-debug \
- --model_save_path ./saved_models/best_model_debug_timestep_changed.pth \
+ --model_save_path ./saved_models/best_model_debug_edit.pth \
  --limit 0.05 \
  --cuda \
  --use_pickle \
- --alignments ./data/Wiki-Data/alignments/ibm_model1_alignments.txt \
- --ref_path debug_ref_al_t.txt \
- --gen_path debug_gen_al_t.txt \
- --unk_gen_path debug_unk_al_t.txt
+ --alignments ./data/Wiki-Data/alignments/combined_alignments_p0_e1.txt \
+ --alignments_pickle ./data/Wiki-Data/alignments/combined_alignments_p0_e1.pickle \
+ --ref_path debug_ref_al_et.txt \
+ --gen_path debug_gen_al_et.txt \
+ --unk_gen_path debug_unk_al_et.txt

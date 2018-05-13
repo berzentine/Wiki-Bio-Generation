8168a502c0ece2aaf64e250acb891774ba0a85c0
Nidhi <nidhi0613@gmail.com>
2018-05-13 00:06:43 -0400

    Minor fix

--- main_dual_alignments_timestep.py
+++ main_dual_alignments_timestep.py
@@ -90,7 +90,7 @@ if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
 
 print("Load data")
-corpus = data_reader.Corpus(data_path, vocab_path, alignment_path, alignment_pickle_path, use_pickle, 1, limit, verbose, use_alignments)
+corpus = data_reader.Corpus(data_path, vocab_path, alignment_path, alignment_pickle_path, use_pickle, 1, limit, verbose, use_alignments=False)
 WORD_VOCAB_SIZE = len(corpus.word_vocab)
 FIELD_VOCAB_SIZE = len(corpus.field_vocab)
 POS_VOCAB_SIZE = len(corpus.pos_vocab)
